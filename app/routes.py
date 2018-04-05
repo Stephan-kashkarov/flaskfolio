dc701104ef38b1dac00a43ddc06cd9d502b791bc
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-04-05 14:02:04 +1000

    adding more html documents

--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,18 @@ def home():
 	user = {'username': 'Admin'}
 	return render_template('index.html', title='Home', user=user)
 
+@app.route('/login')
+def login():
+	return
+
 @app.route('/portfolios')
 def portfolios():
-	return render_template('portfolios.html')
+	return render_template('portfolio.html', title='portfolios')
 
 @app.route('/resumes')
 def resumes():
-	return 'Resumes'
+	return render_template('resumes.html', title='resumes')
 
 @app.route('/donate')
 def donate():
-	return 'support me!'
+	return render_template('donate.html', title='donate')
