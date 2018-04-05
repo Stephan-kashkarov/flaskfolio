04206683322057dc4f5aa36ee770505dbb9f4632
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-04-05 10:01:03 +1000

    worked on making more pages

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from flask import render_template
 from app import app
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def home():
 	user = {'username': 'Admin'}
 	return render_template('index.html', title='Home', user=user)
+
+@app.route('/portfolios')
+def portfolios():
+	return 'Portfolios'
+
+@app.route('/resumes')
+def resumes():
+	return 'Resumes'
+
+@app.route('/donate')
+def donate():
+	return 'support me!'
