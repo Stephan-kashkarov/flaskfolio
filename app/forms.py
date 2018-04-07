7a7e184f60951346324426e5fc803dfa0b705994
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-04-07 10:09:37 +1000

    made login page

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-	user = StringField('username', validators=[DataRequired()])
-	email = StringField('email')
-	password = PasswordField('password', validators=[DataRequired()])
+	user = StringField('*Username', validators=[DataRequired()])
+	email = StringField('Email')
+	password = PasswordField('*Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Login')
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -8,9 +8,15 @@ def home():
 	user = {'username': 'Admin'}
 	return render_template('index.html', title='Home', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for {}, Remember Me = {}' .format(
+			form.user.data,
+			form.remember_me.data
+		))
+		return redirect('/index')
 	return render_template('login.html', title='Login', form=form)
 
 @app.route('/portfolios')
