041971c1d54ab4b1c23f046fdef61801de51964f
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-04-08 18:18:51 +1000

    did everything short of regester

--- app/routes.py
+++ app/routes.py
@@ -10,14 +10,22 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if currnet_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for {}, Remember Me = {}' .format(
-			form.user.data,
-			form.remember_me.data
-		))
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash("invalid username or password")
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
 		return redirect(url_for("home"))
 	return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
 
 @app.route('/regester')
 def regester():
