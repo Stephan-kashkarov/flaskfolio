5f4e744b31a74eec768088434b6fc87f9926f0ce
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-04-10 11:11:11 +1000

    things

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,23 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 from app import app
+from app.models import User, Post
 from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
 	user = {'username': 'Admin'}
 	return render_template('index.html', title='Home', user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if currnet_user.is_authenticated:
+	if current_user.is_authenticated:
 		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.username.data).first()
+		user = User.query.filter_by(username=form.user.data).first()
 		if user is None or not user.check_password(form.password.data):
 			flash("invalid username or password")
 			return redirect(url_for('login'))
@@ -23,6 +26,7 @@ def login():
 	return render_template('login.html', title='Login', form=form)
 
 @app.route('/logout')
+@login_required
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
